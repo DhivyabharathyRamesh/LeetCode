aa31cd45049d46ed53000328b60c0b48c55561fc
Dhivya Bharathy Ramesh <bharathydhivya809@gmail.com>
2026-06-23 22:12:39 +0530

    Time: 27 ms (5.44%), Space: 47.5 MB (5.09%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,49 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        HashMap<Character,Integer> map=new HashMap<>();
+        if(s.length()!=t.length()){
+            return false;
+        }
+        
+       
+        
+        for(int i=0;i<s.length();i++){
+            int count=1;
+            if(map.containsKey(s.charAt(i))){
+                
+                count=(map.get(s.charAt(i)))+1;
+                map.put(s.charAt(i),count);
+            }
+
+            map.put(s.charAt(i),count);
+
+        }
+        for(int i=0;i<s.length();i++){
+            if((map.containsKey(t.charAt(i)))==false){
+                return false;
+
+            }
+            if(map.containsKey(t.charAt(i))){
+                char ch=t.charAt(i);
+                int count=map.get(ch)-1;
+                if(count==0){
+                    map.remove(t.charAt(i));
+                }
+                else{
+                    map.put(ch,count);
+                    }
+                
+                
+            }
+            
+            if(map.size()==0){
+                return true;
+            }
+
+        }
+        return false;
+
+        
+    }
+}
