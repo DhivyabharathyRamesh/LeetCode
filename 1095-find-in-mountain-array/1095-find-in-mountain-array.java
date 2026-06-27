5756e2dd740f7eb8dc641ebc13ba94349d5c8c9c
Dhivya Bharathy Ramesh <bharathydhivya809@gmail.com>
2026-06-27 11:12:17 +0530

    Time: 0 ms (100%), Space: 46.6 MB (60.79%) - LeetHub

---
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -0,0 +1,72 @@
+class Solution {
+
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+
+        int peak = peakIndex(mountainArr);
+
+        // Search in ascending part
+        int firstTry = binarySearch(mountainArr, target, 0, peak, true);
+
+        if (firstTry != -1) {
+            return firstTry;
+        }
+
+        // Search in descending part
+        return binarySearch(mountainArr, target, peak + 1, mountainArr.length() - 1, false);
+    }
+
+    // Find the peak index
+    private int peakIndex(MountainArray mountainArr) {
+
+        int start = 0;
+        int end = mountainArr.length() - 1;
+
+        while (start < end) {
+
+            int mid = start + (end - start) / 2;
+
+            if (mountainArr.get(mid) > mountainArr.get(mid + 1)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+
+    // Binary Search
+    private int binarySearch(MountainArray mountainArr, int target,
+                             int start, int end, boolean isAsc) {
+
+        while (start <= end) {
+
+            int mid = start + (end - start) / 2;
+            int value = mountainArr.get(mid);
+
+            if (value == target) {
+                return mid;
+            }
+
+            if (isAsc) {
+
+                if (target < value) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+
+            } else {
+
+                if (target < value) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+
+            }
+        }
+
+        return -1;
+    }
+}
