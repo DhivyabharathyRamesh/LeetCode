cd236965f266a5d9eb5a017d31fa67e28bf99c41
Dhivya Bharathy Ramesh <bharathydhivya809@gmail.com>
2026-07-06 23:00:54 +0530

    Time: 53 ms (82.85%), Space: 70.4 MB (56.55%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,34 +1,33 @@
 class Solution {
-    public int totalFruit(int[] fruits) {
-        int maxsum=0;
-        int left,right=0;
-        int n=fruits.length;
-        
-        HashMap<Integer,Integer>basket=new HashMap<>();
-        for(left=right=0;right<n;right++){
-            int key=fruits[right];
-            basket.put(
-                key,
-                basket.getOrDefault(key,0)+1
-            );
-            
-            while(basket.size()>2){
-                int lkey =fruits[left];
-                basket.put(
-                    lkey,
-                    basket.get(lkey)-1);
-            
-                if (basket.get(lkey)==0){
-                  basket.remove(lkey);
-              }
-                left++;
+    public int totalFruit(int[] fruits){
+
+    HashMap<Integer,Integer> basket=new HashMap<>();
+
+    int left=0;
+
+    int maxLen=0;
+
+    for(int right=0;right<fruits.length;right++){
+
+        basket.put(fruits[right],
+                basket.getOrDefault(fruits[right],0)+1);
+
+        while(basket.size()>2){
+
+            basket.put(fruits[left],
+                    basket.get(fruits[left])-1);
+
+            if(basket.get(fruits[left])==0){
+
+                basket.remove(fruits[left]);
             }
 
+            left++;
+        }
 
-
-        
-            maxsum=Math.max(maxsum,right-left+1);}
-        return maxsum;
-        
+        maxLen=Math.max(maxLen,right-left+1);
     }
+
+    return maxLen;
+}
 }
