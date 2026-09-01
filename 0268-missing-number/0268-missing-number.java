6bde5ec02683a64e3958e69d55ff28c46c690b67
Dhivya Bharathy Ramesh <bharathydhivya809@gmail.com>
2026-09-01 10:12:11 +0530

    Time: 0 ms (100%), Space: 47.1 MB (82.4%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        
+        int xor=0;
+        for(int i=0;i<nums.length;i++){
+            xor=xor^i;
+            xor=xor^nums[i];
+        }
+        xor=xor^nums.length;
+        return xor;
+
+    }
+}
